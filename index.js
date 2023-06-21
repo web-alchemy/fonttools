00ab11e9e8f2c39c8b7a0c5729f10be284145572
monochromer <monochromer@mail.ru>
2023-06-21 07:18:27 +0500

    refactor to support all CLI options

--- index.js
+++ index.js
@@ -1,34 +1,19 @@
-const { parseArgs } = require('node:util')
-const { subsetFont, subsetFontFromBuffer } = require('./subset.js')
-const { output } = require('./output.js')
+const { preparePyodide } = require('./prepare-pyodide.js')
 
-async function main() {
-  const { values: options } = parseArgs({
-    options: {
-      'input-file': { type: 'string' },
-      'output-file': { type: 'string' },
-      'flavor': { type: 'string' },
-      'text': { type: 'string' },
-      'unicodes': { type: 'string' },
-      'desubroutinize': { type: 'boolean', default: false },
-      'no-hinting': { type: 'boolean', default: false },
-      'layout-features': { type: 'string', default: '*' }
-    },
-    strict: false,
-    allowPositionals: true
-  })
-
-  await subsetFont(options)
-
-  output(options['input-file'], options['output-file'])
-}
-
-module.exports = {
-  main,
-  subsetFont,
-  subsetFontFromBuffer
+async function main(args = process.argv.slice(2)) {
+  const pyodide = await preparePyodide()
+  pyodide.FS.mount(pyodide.FS.filesystems.NODEFS, { root: '.' }, '.')
+  const subsetMainFunc = await pyodide.runPythonAsync(`
+    from fontTools.subset import main
+    main
+  `)
+  subsetMainFunc(args)
 }
 
 if (require.main === module) {
   main()
+}
+
+module.exports = {
+  main
 }
